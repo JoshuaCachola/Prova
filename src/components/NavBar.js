4d533039c29fd27e55cc550f2a2bc0b4c076b3a7
Alan Li <li.alan011@gmail.com>
2020-06-08 16:25:50 -0700

    initial auth0 setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 	const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -9,6 +10,12 @@ const NavBar = () => {
 			{!isAuthenticated && <button onClick={() => loginWithRedirect({})}>Log in</button>}
 
 			{isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+			{isAuthenticated && (
+				<span>
+					<Link to="/">Home</Link>&nbsp;
+					<Link to="/profile">Profile</Link>
+				</span>
+			)}
 		</div>
 	);
 };
