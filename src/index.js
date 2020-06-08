ab2f3ed75c1ddfc9cc3fbede4e1111e4d1fc0516
Alan Li <li.alan011@gmail.com>
2020-06-08 14:36:35 -0700

    basic funcitonality for Auth0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Auth0Provider } from './react-auth0-spa';
+import config from './auth_config.json';
+import history from './utils/history';
+
+// A function that routes the user to the right place after login
+const onRedirectCallback = (appState) => {
+	history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Auth0Provider
+		domain={config.domain}
+		client_id={config.clientId}
+		redirect_uri={window.location.origin}
+		onRedirectCallback={onRedirectCallback}
+	>
+		<App />
+	</Auth0Provider>,
+	document.getElementById('root')
 );
