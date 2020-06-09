f62b985373d6574a9011d551223175d4752d5825
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-06-09 17:32:22 -0400

    Various things

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import NavBar from './components/NavBar';
 
 import { Router, Route, Switch } from 'react-router-dom';
-import CreateMap from './components/routes/CreateMap';
+import CreateRoute from './components/routes/CreateRoute';
 import LandingPage from './components/LandingPage';
 import HomePage from './components/HomePage';
 import MyRoutes from './components/routes/MyRoutes';
@@ -27,7 +27,7 @@ function App() {
 					<PrivateRoute path="/profile" exact component={Profile} />
 					<PrivateRoute path="/external-api" exact component={ExternalApi} />
 					<Route path="/" exact={true} component={LandingPage} />
-					<Route path="/create-route" exact={true} component={CreateMap} />
+					<Route path="/create-route" exact={true} component={CreateRoute} />
 					<Route path="/home" exact={true} component={HomePage} />
 					<Route path="/my-routes" exact={true} component={MyRoutes} />
 					<Route path="/my-stats" exact={true} component={MyStats} />
