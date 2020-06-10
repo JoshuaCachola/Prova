31c6be7de00823bf879ed89aa30b8aa2a19bc1b2
Alan Li <li.alan011@gmail.com>
2020-06-10 14:26:24 -0700

    profile now uses the redux state of user

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,21 +3,20 @@ import { useSelector } from 'react-redux';
 import { useAuth0 } from '../react-auth0-spa';
 
 const Profile = () => {
-	const { loading, user } = useAuth0();
-	// const user = useSelector((state) => state.authorization.currentUser);
-	// console.log('profile: ', currentUser);
+	const { loading } = useAuth0();
+	const currentUser = useSelector((state) => state.authorization.currentUser);
 
-	if (loading || !user) {
+	if (loading || !currentUser) {
 		return <div>Loading...</div>;
 	}
 	return (
 		<Fragment>
-			{user && (
+			{currentUser && (
 				<Fragment>
-					<img src={user.picture} alt="Profile" />
-					<h2>{user.name}</h2>
-					<p>{user.email}</p>
-					<code>{JSON.stringify(user, null, 2)}</code>
+					<img src={currentUser.picture} alt="Profile" />
+					<h2>{currentUser.name}</h2>
+					<p>{currentUser.email}</p>
+					<code>{JSON.stringify(currentUser, null, 2)}</code>
 				</Fragment>
 			)}
 		</Fragment>
