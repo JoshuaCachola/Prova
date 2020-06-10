e493e648354cb2cfa9ee765c1528ade01e749d5a
Alan Li <li.alan011@gmail.com>
2020-06-10 14:11:23 -0700

    fix navbar. but profile redux state is still not working

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,20 +3,21 @@ import { useSelector } from 'react-redux';
 import { useAuth0 } from '../react-auth0-spa';
 
 const Profile = () => {
-	const { loading } = useAuth0();
-	const currentUser = useSelector((state) => state.authorization.currentUser);
+	const { loading, user } = useAuth0();
+	// const user = useSelector((state) => state.authorization.currentUser);
+	// console.log('profile: ', currentUser);
 
-	if (loading || !currentUser) {
+	if (loading || !user) {
 		return <div>Loading...</div>;
 	}
 	return (
 		<Fragment>
-			{currentUser && (
+			{user && (
 				<Fragment>
-					<img src={currentUser.picture} alt="Profile" />
-					<h2>{currentUser.name}</h2>
-					<p>{currentUser.email}</p>
-					<code>{JSON.stringify(currentUser, null, 2)}</code>
+					<img src={user.picture} alt="Profile" />
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+					<code>{JSON.stringify(user, null, 2)}</code>
 				</Fragment>
 			)}
 		</Fragment>
