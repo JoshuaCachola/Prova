1af14abfcc40253a4ef581198ecf31764ce2f869
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-06-13 10:47:07 -0700

    added total stats page

---
+++ src/components/stats/TotalStats.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+const TotalStats = ({ runs }) => {
+  return (
+    <CountUp
+      start={0.00}
+      end={}
+      delay={0}
+    >
+
+    </CountUp>
+  );
+};
+
+export default TotalStats;
